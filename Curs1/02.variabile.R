edc224f01143646001f7554aaf838165d236cb66
mara <marag273@gmail.com>
2025-08-17 02:09:18 +0300

    intro R

---
+++ Curs1/02.variabile.R
@@ -0,0 +1,13 @@
+
+# asignare a unei variabile
+a <- 3
+print(a)
+
+# asignare inversa
+33 -> b 
+print(b)
+
+c <- "Rosu"
+print(c)
+
+cat(b, c, a)
---
+++ Curs1/04.operatii.R
@@ -0,0 +1,19 @@
+
+a <- 10
+b <- 5
+
+print(a + b)
+print(a - b)
+
+# ridicare la putere
+print(a ^ b)
+
+# radical
+print(sqrt(81))
+
+
+print(10/3)
+
+print(10 %% 3)  # restul impartirii
+
+print(10 %/% 3) # catul impartirii fara rest
